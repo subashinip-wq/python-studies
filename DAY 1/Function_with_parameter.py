6562fdb7abf3f939990cafbf498c76b3fb306e96
subashinip-wq <subashinip@karunya.edu.in>
2026-06-03 11:36:29 +0530

    Add function to show admission details

---
+++ DAY 1/Function_with_parameter.py
@@ -0,0 +1,8 @@
+def show_admission(name, course, city):
+    print("---- Admission Details ----")
+    print(f"Student : {name}")
+    print(f"Course  : {course}")
+    print(f"City    : {city}")
+    
+show_admission("subashini", "B.Tech", "Chennai")
+show_admission("bablu", "AIML", "Coimbatore")
