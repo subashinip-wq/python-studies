f7ec3630bcf3a82aaae219adf6b06e6feb8f6302
subashinip-wq <subashinip@karunya.edu.in>
2026-06-03 11:37:20 +0530

    Add function with default parameter for course

---
+++ DAY 1/Function_default_parameter.py
@@ -0,0 +1,5 @@
+def show_admission(name, city, course="Not Selected"):
+    print(f"{name} from {city}: {course}")
+
+show_admission("Kiran", "Madurai")            
+show_admission("Anbu", "Salem", "B.Com")        
