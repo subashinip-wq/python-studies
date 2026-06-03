4401d9fa8b540eb0b671b0c765f28f13e7e2ee9a
subashinip-wq <subashinip@karunya.edu.in>
2026-06-03 11:33:34 +0530

    Add simple_variables.py for user input and output

---
+++ DAY 1/simple_variables.py
@@ -0,0 +1,12 @@
+# Get details from user
+
+name = input("Enter Student Name: ")
+course = input("Enter Course Name: ")
+city = input("Enter City: ")
+
+# Print details
+
+print("\n--- Admission Details ---")
+print("Student Name:", name)
+print("Course:", course)
+print("City:", city)
