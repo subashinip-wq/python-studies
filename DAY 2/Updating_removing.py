8e1d3e652ca1804753b29a3fff0222ed3ecf43a9
subashinip-wq <subashinip@karunya.edu.in>
2026-06-03 17:27:58 +0530

    Add examples for updating and removing list items
    Demonstrate updating and removing elements from a list.

---
+++ DAY 2/Updating_removing.py
@@ -0,0 +1,13 @@
+UPDATING A LIST:
+
+patients = ["Priya", "Ravi", "Kiran"]
+patients[1] = "Anbu"
+print(patients)
+
+REMOVING A LIST:
+
+patients = ["Priya", "Ravi", "Kiran"]
+patients.remove("Ravi")
+print(patients)
+patients.pop(0)
+print(patients)
