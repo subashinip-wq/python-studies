adf72fb92a65939ee2babeb05008be11644aa09e
subashinip-wq <subashinip@karunya.edu.in>
2026-06-03 17:30:13 +0530

    Add show_patients function to display patient list
    Added a function to display the total number of patients and list their names.

--- DAY 2/Adding_function.py
+++ DAY 2/Adding_function.py
@@ -5,3 +5,14 @@ patients.append("kanya")
 patients.insert(0, "karthika")
 
 print(patients)
+
+FUNCTION WITH LIST:
+def show_patients(patient_list):
+    print("Total Patients:", len(patient_list))
+
+    for patient in patient_list:
+        print("-", patient)
+
+my_patients = ["Priya", "Ravi", "Kiran"]
+
+show_patients(my_patients)
