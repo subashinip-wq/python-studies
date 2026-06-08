cf828bbc461891dd6e96f8548ec9cb67edee3a5d
subashinip-wq <subashinip@karunya.edu.in>
2026-06-08 19:17:29 +0530

    Add patient dictionary and print details

---
+++ DAY 3/EXERCISE 5.py
@@ -0,0 +1,9 @@
+patient = {
+    "patient_name": "Godson",
+    "hospital": "Apollo",
+    "disease": "Fever",
+    "doctor": "Dr. Kumar"
+}
+
+for key, value in patient.items():
+    print(key, ":", value)
